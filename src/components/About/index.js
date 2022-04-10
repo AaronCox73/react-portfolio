2c69c8cc7e03fb3d0bdebfa8a91c0183aaea815b
Aaron Cox <aaroncox@Aarons-iMac.local>
2022-04-10 11:44:25 -0600

    figured out 'bug'

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import ProfilePic from '../../assets/profilePic.jpeg'
-import './About.css';
+import "./about.css"
 import SLC from '../../assets/slc.jpeg'
 
 function About() {
     return (
-        <section className='about-me'>
-            <div className="container">
+        <section>
+            <div className='about-me'>
                 <img src={SLC} className="aboutBackground" />
+
                 <p className="aboutInfo">
-                    Hello there, my name is Aaron Jefferson Cox but you should call me AJ. 
-                    I am a college-educated-knuckle-bashing-blue-collar worker who traded his wrenchs for a keyboard and a mouse. 
+                    Hello there, my name is Aaron Jefferson Cox but you should call me AJ.
+                    I am a college-educated-knuckle-bashing-blue-collar worker who traded his wrenchs for a keyboard and a mouse.
                     I have discovered a passion for software engineering and that is what you can find me doing with most of my time.
-                     
-                  
-            </p>
-                {/* <img src={ProfilePic} /> */}
+                </p>
 
             </div>
-        </section>
+        </section >
 
     )
 }
