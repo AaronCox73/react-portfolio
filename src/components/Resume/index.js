6816dd8dc3c6e28f6a81aa205147c25b4b7a92f3
Aaron Cox <aaroncox@aarons-imac.lan>
2022-08-19 19:11:53 -0600

    almost done

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../Resume/resume.css'
 import resume from '../../assets/resume.png'
+
 
 function Resume() {
     return (
         <section className='resume'>
-            <h2>Resume</h2>
+            <h2 id ='resumeTitle'>Resume</h2>
             <p id='about' >
                 download my <a href={resume} download>Resume</a>
             </p>
